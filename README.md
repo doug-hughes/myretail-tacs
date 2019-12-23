d03ef35b61ceb2f0d631062375dc6fa00a447221
doug-hughes <52683529+hughey70@users.noreply.github.com>
2019-12-23 12:36:42 -0600

    applying transient changes before fixing test classpath

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ A PoC RESTful service that combines product information with pricing
 
 myRetail was developed with [STS](https://spring.io/tools/sts) with the [Eclipse Buildship plugin](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwjKz4G12MnmAhWSFTQIHfoABc8QFjAAegQIChAC&url=https%3A%2F%2Fprojects.eclipse.org%2Fprojects%2Ftools.buildship&usg=AOvVaw3w79u_pTg-smfQ0zfK5PIN) and can be imported as a gradle project. The project will automatically build as part of the import
 
-To run the project outside of STS, run the `./gradlew bootRun` from the project directory
+To run the project outside of STS:
+
+1.  run the `./gradlew startMongoDb` from the project directory
+2.  run the `./gradlew bootRun` from the project directory
 
--- src/main/java/tacs/myretail/AppConfig.java
+++ src/main/java/tacs/myretail/AppConfig.java
@@ -1,9 +1,10 @@
 package tacs.myretail;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilderFactory;
 
 @Configuration
 public class AppConfig {
@@ -13,4 +14,9 @@ public class AppConfig {
 //		return builder.rootUri("http://redsky.target.com/v2/pdp/tcin") //13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics)
 //				.build();
 //	}
+	@Bean
+	public WebClient webClient() {
+		UriBuilderFactory ubf = new DefaultUriBuilderFactory("http://redsky.target.com/v2/pdp/tcin/{tcin}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics");
+		return WebClient.builder().uriBuilderFactory(ubf).build();
+	}
 }

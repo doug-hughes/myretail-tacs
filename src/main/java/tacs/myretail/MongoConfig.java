8a48af2b1e2d16f9e21ac6975ae1c0b1ca4c02be
doug-hughes <52683529+hughey70@users.noreply.github.com>
2020-01-01 15:09:30 -0600

    cleanup mongoconfig class

--- src/main/java/tacs/myretail/MongoConfig.java
+++ src/main/java/tacs/myretail/MongoConfig.java
@@ -1,25 +1,16 @@
 package tacs.myretail;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.index.IndexOperations;
-import org.springframework.data.mongodb.core.index.IndexResolver;
-import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
-import tacs.myretail.model.Price;
-
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration implements CommandLineRunner {
+public class MongoConfig extends AbstractMongoClientConfiguration {
 	@Autowired
 	MongoMappingContext mongoMappingContext;
 	@Autowired
@@ -33,19 +24,5 @@ public class MongoConfig extends AbstractMongoClientConfiguration implements Com
 	@Override
 	protected String getDatabaseName() {
 		return "myretail";
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void initIndicesAfterStartup() {
-		IndexOperations indexOps = mongoTemplate.indexOps(Price.class);
-
-		IndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
-		resolver.resolveIndexFor(Price.class).forEach(indexOps::ensureIndex);
 	}
 }

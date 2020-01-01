79fea2348c7cfa27c29de183eae2f048c112dd98
doug-hughes <52683529+hughey70@users.noreply.github.com>
2020-01-01 17:14:58 -0600

    log request to logger instead of stdout

--- src/main/java/tacs/myretail/AppConfig.java
+++ src/main/java/tacs/myretail/AppConfig.java
@@ -1,5 +1,7 @@
 package tacs.myretail;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
@@ -10,6 +12,7 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 public class AppConfig {
+	private static final Logger log = LogManager.getLogger();
 	private final String REDSKY_TCIN = "https://redsky.target.com/v2/pdp/tcin/{tcin}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
 
 	@Bean
@@ -20,10 +23,9 @@ public class AppConfig {
 
 	private ExchangeFilterFunction logRequest() {
 		return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-			System.out.printf("Request: {%s} {%s}", clientRequest.method(), clientRequest.url());
+			log.debug(String.format("Request: {%s} {%s}", clientRequest.method(), clientRequest.url()));
 			clientRequest.headers()
-					.forEach((name, values) -> values.forEach(value -> System.out.printf("{%s}={%s}", name, value)));
-			System.out.println();
+					.forEach((name, values) -> values.forEach(value -> log.trace(String.format("{%s}={%s}", name, value))));
 			return Mono.just(clientRequest);
 		});
 	}

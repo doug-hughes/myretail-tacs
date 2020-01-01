6173b10b6de3065e7c2e5aa0a1f343a7a58477f4
doug-hughes <52683529+hughey70@users.noreply.github.com>
2019-12-31 19:51:26 -0600

    #3pb426
    skip duplicate key error when populating with dev endpoint

--- src/main/java/tacs/myretail/dev/DevController.java
+++ src/main/java/tacs/myretail/dev/DevController.java
@@ -3,13 +3,18 @@ package tacs.myretail.dev;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import com.mongodb.MongoWriteException;
 
 import tacs.myretail.dev.SearchResponse.Item;
 import tacs.myretail.dev.SearchResponse.Items;
@@ -18,6 +23,7 @@ import tacs.myretail.model.PriceRepository;
 
 @RestController
 public class DevController {
+	private static final Logger log = LogManager.getLogger();
 	private static final String REDSKY_TCIN = "https://redsky.target.com/v2/plp/search/?channel=web&count=24&default_purchasability_filter=true&facet_recovery=false&isDLP=false&offset=0&pageId=%2Fs%2Ffootball&pricing_store_id=663&scheduled_delivery_store_id=663&store_ids=663%2C1068%2C1211%2C2449%2C1272&visitorId=016F4998B7530201A7E92CCFB1872770&include_sponsored_search_v2=true&ppatok=AOxT33a&platform=desktop&useragent=Mozilla&key=eb2551e4accc14f38cc42d32fbc2b2ea&excludes=taxonomy,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
 	@Autowired
 	private PriceRepository priceRepository;
@@ -33,8 +39,13 @@ public class DevController {
 		List<Integer> savedTcins = new ArrayList<>();
 		for (Item item : qryResults.getItem()) {
 			if (item.getTcin() > 0 && item.getCurrentRetail() != null) {
-				Price saved = priceRepository.save(new Price(item.getTcin(), item.getCurrentRetail(), "USD"));
-				savedTcins.add(saved.getTcin());
+				try {
+					Price saved = priceRepository.save(new Price(item.getTcin(), item.getCurrentRetail(), "USD"));
+					savedTcins.add(saved.getTcin());
+				} catch (MongoWriteException | DuplicateKeyException mwe) {
+					// probably duplicate error, just log and continue
+					log.warn("Error encountered trying to save price for {}", item.getTcin(), mwe);
+				}
 			}
 		}
 		ResponseEntity<List<Integer>> response = ResponseEntity.ok(savedTcins);

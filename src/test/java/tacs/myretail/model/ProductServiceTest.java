7b407593198946588869f764ce0cf3e192dcaa8b
doug-hughes <52683529+hughey70@users.noreply.github.com>
2020-01-06 15:28:36 -0600

    #3rc382
    Finished up unit tests for ProductService

--- src/test/java/tacs/myretail/model/ProductServiceTest.java
+++ src/test/java/tacs/myretail/model/ProductServiceTest.java
@@ -3,10 +3,11 @@ package tacs.myretail.model;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,14 +16,13 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -46,7 +46,7 @@ public class ProductServiceTest {
 	private static int mockPort;
 	@MockBean
 	private PriceRepository priceRepository;
-	@Autowired
+	@Autowired() // use @InjectMocks to include mocking productWebClient
 	private ProductService service;
 
 	private ProductService getService() {
@@ -75,11 +75,9 @@ public class ProductServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
-		this.priceRepository = mock(PriceRepository.class);
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		// Without this method mockbean priceRepository is not available in
+		// ProductService
+		MockitoAnnotations.initMocks(this);
 	}
 
 	/**************************** INPUT VALIDATION *******************************/
@@ -104,6 +102,41 @@ public class ProductServiceTest {
 	public void givenItemIdLargerThanInteger_WhenFindItemByTCIN_ThenEmpty() throws Exception {
 		// Given
 		String id = String.valueOf(Long.MAX_VALUE);
+		enqueueProductNotFound(getServer());
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findItemByTCIN(id);
+			}
+		};
+
+		// Then
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
+	}
+
+	@Test(timeout = 20000)
+	public void givenItemIdSpace_WhenFindPriceByTCIN_ThenEmpty() throws Exception {
+		// Given
+		String id = " ";
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findPriceByTCIN(id);
+			}
+		};
+
+		// Then
+		assertThatExceptionOfType(NumberFormatException.class).isThrownBy(tc);
+	}
+
+	@Test(timeout = 20000)
+	public void givenItemIdSpace_WhenFindItemByTCIN_ThenEmpty() throws Exception {
+		// Given
+		String id = " ";
 		getServer().enqueue(jsonResponse(404).setBody("{\"product\":{\"item\":{}}}"));
 
 		// When
@@ -118,174 +151,306 @@ public class ProductServiceTest {
 		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
-	public void givenItemIdSpace_ThenReturn404() throws Exception {
-		// Given
-		String id = " ";
-
-		// Then
-//		getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isNotFound().expectBody().isEmpty();
-	}
-
-	@Ignore
-	@Test(timeout = 20000)
-	public void givenItemIdNotInteger_ThenReturn404() throws Exception {
+	public void givenItemIdNotInteger_WhenFindPriceByTCIN_ThenEmpty() throws Exception {
 		// Given
 		String id = "notinteger";
 
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findPriceByTCIN(id);
+			}
+		};
+
 		// Then
-//		getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isNotFound().expectBody().isEmpty();
+		assertThatExceptionOfType(NumberFormatException.class).isThrownBy(tc);
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
-	public void givenItemIdZero_ThenReturn404() throws Exception {
+	public void givenItemIdNotInteger_WhenFindItemByTCIN_ThenEmpty() throws Exception {
 		// Given
-		String id = "0";
+		String id = "notinteger";
+		getServer().enqueue(jsonResponse(404).setBody("{\"product\":{\"item\":{}}}"));
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findItemByTCIN(id);
+			}
+		};
 
 		// Then
-//		getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isNotFound().expectBody().isEmpty();
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
+	}
+
+	@Test(timeout = 20000)
+	public void givenItemIdZero_WhenFindPriceByTCIN_ThenEmpty() throws Exception {
+		// Given
+		when(getRepository().findByTcin(Mockito.anyInt())).thenReturn(Optional.empty());
+		String id = "0";
+
+		// When
+		Optional<Price> price = getService().findPriceByTCIN(id);
+
+		// Then
+		assertTrue(price.isEmpty());
+	}
+
+	@Test(timeout = 20000)
+	public void givenItemIdZero_WhenFindItemByTCIN_ThenEmpty() throws Exception {
+		// Given
+		String id = "0";
+		getServer().enqueue(jsonResponse(404).setBody("{\"product\":{\"item\":{}}}"));
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findItemByTCIN(id);
+			}
+		};
+
+		// Then
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
 	}
 
 	// ============================ NOT OUR CONTROLLER
-	@Ignore
 	@Test(timeout = 20000)
-	public void givenItemIdNull_ThenReturn404() throws Exception {
+	public void givenItemIdNull_WhenFindPriceByTCIN_ThenEmpty() throws Exception {
 		// Given
 		String id = null;
 
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findPriceByTCIN(id);
+			}
+		};
+
 		// Then
-//		getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isNotFound().expectHeader()
-//				.valueEquals("Content-Type", "application/json").expectBody()
-//				.json("{\"status\":404,\"error\":\"Not Found\",\"message\":\"No message available\",\"path\":\"/products/\"}");
+		assertThatExceptionOfType(NumberFormatException.class).isThrownBy(tc);
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
-	public void givenItemIdEmpty_ThenReturn404() throws Exception {
+	public void givenItemIdNull_WhenFindItemByTCIN_ThenEmpty() throws Exception {
+		// Given
+		String id = null;
+		getServer().enqueue(jsonResponse(404).setBody("{\"product\":{\"item\":{}}}"));
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findItemByTCIN(id);
+			}
+		};
+
+		// Then
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
+	}
+
+	@Test(timeout = 20000)
+	public void givenItemIdEmpty_WhenFindPriceByTCIN_ThenEmpty() throws Exception {
 		// Given
 		String id = "";
 
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findPriceByTCIN(id);
+			}
+		};
+
 		// Then
-//		getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isNotFound().expectHeader()
-//				.valueEquals("Content-Type", "application/json").expectBody()
-//				.json("{\"status\":404,\"error\":\"Not Found\",\"message\":\"No message available\",\"path\":\"/products/\"}");
+		assertThatExceptionOfType(NumberFormatException.class).isThrownBy(tc);
+	}
+
+	@Test(timeout = 20000)
+	public void givenItemIdEmpty_WhenFindItemByTCIN_ThenEmpty() throws Exception {
+		// Given
+		String id = "";
+		getServer().enqueue(jsonResponse(404).setBody("{\"product\":{\"item\":{}}}"));
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findItemByTCIN(id);
+			}
+		};
+
+		// Then
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
 	}
 
 	/************************ END INPUT VALIDATION *******************************/
 
-	@Ignore
 	@Test(timeout = 20000)
-	public void givenItemIdNotExistAndPriceNotExist_ThenReturn404() throws Exception {
+	public void givenItemIdNotExistAndPriceNotExist_WhenFindByTcin_ThenNotFound() throws Exception {
 		// Given
-		String id = "15117729";
+		String id = "13860428";
+		when(getRepository().findByTcin(Mockito.anyInt())).thenReturn(Optional.empty());
+		enqueueProductNotFound(server);
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findByTcin(id);
+			}
+		};
 
 		// Then
-//		getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isNotFound().expectBody().isEmpty();
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
-	public void givenItemIdNotExistAndPriceExists_WhenFindByTCIN_ThenEmpty() throws Exception {
-		Price currentPrice = null;
-		try {
+	public void givenItemIdNotExistAndPriceExists_WhenFindByTcin_ThenEmpty() throws Exception {
+		// Given
+		String id = "13860428";
+		BigDecimal value = BigDecimal.valueOf(15.99);
+		String currency = "JPY";
+		when(getRepository().findByTcin(Mockito.anyInt()))
+				.thenReturn(Optional.of(new Price(Integer.valueOf(id), value, currency)));
+		enqueueProductNotFound(server);
 
-			// Given
-			String id = "15117729";
-
-			// When
-			BigDecimal value = BigDecimal.valueOf(12.99);
-//			currentPrice = savePrice(new Price(Integer.valueOf(id), value, "USD"));
-
-			// Then
-//			getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isNotFound().expectBody()
-//					.isEmpty();
-
-		} finally {
-			// Cleanup
-			if (currentPrice != null) {
-//				getPriceRepository().delete(currentPrice);
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findByTcin(id);
 			}
-		}
+		};
+
+		// Then
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
 	public void givenItemIdAndPriceExists_WhenFindByTCIN_ThenReturnSingleProductWithPrice() throws Exception {
-		Price currentPrice = null;
-		try {
-			// Given
-			String id = "26396662";
+		// Given
+		String id = "13860428";
+		BigDecimal value = BigDecimal.valueOf(15.99);
+		String currency = "JPY";
+		when(getRepository().findByTcin(Mockito.anyInt()))
+				.thenReturn(Optional.of(new Price(Integer.valueOf(id), value, currency)));
+		enqueueValidProduct(server);
 
-			// When
-			BigDecimal value = BigDecimal.valueOf(15.99);
-//			currentPrice = savePrice(new Price(Integer.valueOf(id), value, "USD"));
+		// When
+		Product product = getService().findByTcin(id);
 
-			// Then
-//			getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isOk().expectHeader()
-//					.valueEquals("Content-Type", "application/json").expectBody()
-//					.json("{\"id\":26396662,\"name\":\"Exploding Kittens Game\",\"current_price\":{\"value\":15.99,\"currency_code\":\"USD\"}}");
-
-		} finally {
-			// Cleanup
-			if (currentPrice != null) {
-//				getPriceRepository().delete(currentPrice);
-			}
-		}
+		// Then
+		assertTrue(product.getCurrent_price().isPresent());
+		assertEquals("The Big Lebowski (Blu-ray)", product.getName());
+		assertEquals(13860428L, product.getId());
+		assertEquals(value, product.getCurrent_price().get().getValue());
+		assertEquals(currency, product.getCurrent_price().get().getCurrency_code());
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
 	public void givenItemPriceExists_WhenFindPriceByTCIN_ThenReturnPrice() throws Exception {
+		// Given
+		String id = "13860428";
+		BigDecimal value = BigDecimal.valueOf(15.99);
+		String currency = "JPY";
+		when(getRepository().findByTcin(Mockito.anyInt()))
+				.thenReturn(Optional.of(new Price(Integer.valueOf(id), value, currency)));
+
+		// When
+		Optional<Price> price = getService().findPriceByTCIN(id);
+
+		// Then
+		assertTrue(price.isPresent());
+		assertEquals((int) Integer.valueOf(id), price.get().getTcin());
+		assertEquals(value, price.get().getValue());
+		assertEquals(currency, price.get().getCurrency_code());
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
 	public void givenItemIdExists_WhenFindItemByTCIN_ThenReturnItem() throws Exception {
+		// Given
+		String id = "13860428";
+		enqueueValidProduct(getServer());
+
+		// When
+		Product product = getService().findByTcin(id);
+
+		// Then
+		assertTrue(product.getCurrent_price().isEmpty());
+		assertEquals("The Big Lebowski (Blu-ray)", product.getName());
+		assertEquals(13860428L, product.getId());
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
 	public void givenItemPriceMissing_WhenFindPriceByTCIN_ThenReturnEmpty() throws Exception {
+		// Given
+		String id = "13860428";
+		when(getRepository().findByTcin(Mockito.anyInt())).thenReturn(Optional.empty());
+
+		// When
+		Optional<Price> price = getService().findPriceByTCIN(id);
+
+		// Then
+		assertTrue(price.isEmpty());
 	}
 
-	@Ignore
 	@Test(timeout = 20000)
-	public void givenItemIdNotFound_WhenFindItemByTCIN_ThenEmpty() throws Exception {
+	public void givenItemIdNotFound_WhenFindItemByTCIN_ThenNotFound() throws Exception {
+		// Given
+		String id = "13860428";
+		enqueueProductNotFound(server);
+
+		// When
+		ThrowingCallable tc = new ThrowingCallable() {
+			@Override
+			public void call() throws Throwable {
+				getService().findByTcin(id);
+			}
+		};
+
+		// Then
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(tc);
 	}
 
 	@Test()
 	public void givenItemIdAndPriceNotExist_WhenFindByTCIN_ThenReturnSingleProductWithoutPrice() throws Exception {
 		// Given
 		when(getRepository().findByTcin(Mockito.anyInt())).thenReturn(Optional.empty());
-
 		String id = "13860428";
+		enqueueValidProduct(getServer());
 
 		// When
+		Product product = getService().findByTcin(id);
+
+		// Then
+		assertTrue(product.getCurrent_price().isEmpty());
+		assertEquals("The Big Lebowski (Blu-ray)", product.getName());
+		assertEquals(13860428L, product.getId());
+	}
+
+	private static MockResponse jsonResponse(int code) {
+		return new MockResponse().setResponseCode(code).setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+	}
+
+	/** mocked lookup returns single valid item with tcin:"13860428" **/
+	private void enqueueValidProduct(MockWebServer server) throws URISyntaxException, IOException {
 		/* Get the response for the item lookup from file */
 		String resourceFileName = "RestClientTest-givenItemIdAndPriceNotExist_ThenReturnSingleProduct.json";
 		URL url = getClass().getClassLoader().getResource(resourceFileName);
 		Path path = Paths.get(url.toURI());
 		String jsonBody = Files.readString(path);
 		/* return response in item lookup */
-		getServer().enqueue(jsonResponse(200).setBody(jsonBody));
-
-		// Then
-		Product product = getService().findByTcin(id);
-		
-		assertTrue(product.getCurrent_price().isEmpty());
-		assertEquals("The Big Lebowski (Blu-ray)", product.getName());
-		assertEquals(13860428L, product.getId());
-		
-//		getWebTestClient().get().uri("/products/{id}", id).exchange().expectStatus().isOk().expectHeader()
-//				.valueEquals("Content-Type", "application/json").expectBody()
-//				.json("{\"id\":13860428,\"name\":\"The Big Lebowski (Blu-ray)\"}");
-
+		server.enqueue(jsonResponse(200).setBody(jsonBody));
 	}
 
-	private static MockResponse jsonResponse(int code) {
-		return new MockResponse().setResponseCode(code).setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+	/** mocked lookup returns 404 with skeletal body **/
+	private void enqueueProductNotFound(MockWebServer server) {
+		server.enqueue(jsonResponse(404).setBody("{\"product\":{\"item\":{}}}"));
 	}
 
 	@Configuration
